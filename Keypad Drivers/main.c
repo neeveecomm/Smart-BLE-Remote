06b42cf62eaedf9b2778fcd2649d518883d9822f
Sathish kumar S <154660384+Sathishkumar-neeveetech@users.noreply.github.com>
2024-03-14 02:23:37 +0530

    Updated drivers

--- Keypad Drivers/main.c
+++ Keypad Drivers/main.c
@@ -1,39 +1,77 @@
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/i2c.h>
-#include <zephyr/devicetree.h>
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/drivers/gpio/gpio_sx1509b.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
+#include <soc.h>
+#include <zephyr/drivers/kscan.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/kscan_sx1508.h>
+
+#define LOG_LEVEL LOG_LEVEL_DBG
+#include <zephyr/logging/log.h>
 
 
-#define I2C0_NODE DT_NODELABEL(sx1509b)
-
-static const struct i2c_dt_spec i2c_node = I2C_DT_SPEC_GET(I2C0_NODE);
-
-static struct device *sx = DEVICE_DT_GET(DT_NODELABEL(sx1509b));                   
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 
-int gpio_sx1509b_callback(struct device *dev,int key)
-{  
-         printk("keypad data%d\n",key);
+#define I2C0_NODE DT_NODELABEL(sx1508)
 
-}
+static const struct  i2c_dt_spec i2c_node =I2C_DT_SPEC_GET(I2C0_NODE);
+
+static struct device *dev = DEVICE_DT_GET(DT_NODELABEL(sx1508)); 
+
+
+#define key_press_detect DT_NODELABEL(sx1508)
+
+static const  struct gpio_dt_spec irq = GPIO_DT_SPEC_GET( key_press_detect, irq_gpios);
+
+
+ void keyscan_callback(const struct device *dev, uint32_t row,
+			     uint32_t column, bool pressed)
+{
+	printk("button pressed\n");
+} 
+
+int ret;
+
+
 int main()
 {
+	int err;
 
-        if (!device_is_ready(sx))
-        {
-                printk("dev bus %s is not ready! \n\r", sx->name);
-        }
+	// printk("program start\n");
 
-        if (!device_is_ready(i2c_node.bus))
-        {
+    // const struct device *const dev = DEVICE_DT_GET(I2C0_NODE);
+    
+    if (!device_is_ready(dev)) {
+		LOG_ERR("Keyscan device not ready\n");
+		return 0;
+	}
+	
+	if (!device_is_ready(i2c_node.bus)){
                 printk("I2C bus %s is not ready! \n\r", i2c_node.bus->name);
         }
-          
+		
+        
+	if (!device_is_ready(irq.port)){
+                printf("pin is not ready\n");
+        }
 
-        sx1509_add_callback(sx,gpio_sx1509b_callback);
 
-        return 0;
+
+	err = kscan_config(dev,keyscan_callback);
+
+	if (err) {
+		printk("Failed to add keyscan callback err %d\n", err);
+	}
+
+
+    return 0;
 }
--- Keypad Drivers/sx1508.h
+++ Keypad Drivers/sx1508.h
@@ -1,48 +1,25 @@
 /*
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2019 Henrik Brix Andersen <henrik@brixandersen.dk>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef ZEPHYR_INCLUDE_DRIVERS_GPIO_GPIO_SX1509B_H_
-#define ZEPHYR_INCLUDE_DRIVERS_GPIO_GPIO_SX1509B_H_
 
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
+#ifndef ZEPHYR_INCLUDE_DRIVERS_KEYPAD_SX1508_H_
+#define ZEPHYR_INCLUDE_DRIVERS_KEYPAD_SX1508_H_
 
-
-
-/* callback function */
-typedef int (*button_user_cb_t)(struct device* dev,int key_rx);
-
-__subsystem struct callback_driver_api {
-	button_user_cb_t usr_cb;
-};
+#include <zephyr/drivers/kscan.h>
 
 /**
- * @brief Add an application callback.
- * @param port Pointer to the device structure for the driver instance.
- * @param callback A valid Application's callback structure pointer.
+ * Register a SX1508 keyscan device to be notified of relevant
+ * keyscan events by the keyscan interrupt thread in the SX1508
+ * parent driver.
+ *
+ * @param parent SX1508 parent device.
+ * @param callback Keyscan callback function.
  * @return 0 if successful, negative errno code on failure.
- *
- * @note Callbacks may be added to the device from within a callback
- * handler invocation, but whether they are invoked for the current
- * GPIO event is not specified.
- *
- * Note: enables to add as many callback as needed on the same port.
  */
-static inline int sx1509_add_callback(const struct device *port,
-				    button_user_cb_t *callback)
-{
-	const struct callback_driver_api *api =
-		(const struct callback_driver_api *)port->api;
+static int sx1508_register_keyscan_callback(const struct device *parent,
+				                            kscan_callback_t callback);
 
-	if (api->usr_cb == NULL) {
-		return -ENOTSUP;
-	}
-
-	return api->usr_cb(port, callback);
-}
-
-
-#endif /* ZEPHYR_INCLUDE_DRIVERS_GPIO_GPIO_SX1509B_H_ */
+#endif /* ZEPHYR_INCLUDE_DRIVERS_KEYPAD_SX1508 */
