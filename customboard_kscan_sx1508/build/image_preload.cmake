514f66240deb0eacfbebb11f48a1b4b9b0cef927
Sathish kumar S <154660384+Sathishkumar-neeveetech@users.noreply.github.com>
2024-03-20 21:07:49 +0530

    custom_board

---
+++ customboard_kscan_sx1508/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "neevee_remote_52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/projects/smart_ble_remote;c:/projects;c:/projects/kscan_sx1508;C:/ncs/v2.5.0/nrf;C:/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/projects/kscan_sx1508/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/projects/kscan_sx1508/neevee_remote_52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
