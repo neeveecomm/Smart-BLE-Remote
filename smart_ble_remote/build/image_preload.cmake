2264459493b8ea2a2496988010fd9ab25feafbc2
Sathish kumar S <154660384+Sathishkumar-neeveetech@users.noreply.github.com>
2024-04-29 21:19:05 +0530

    Driver_updated

--- smart_ble_remote/build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "smart-remote-52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/projects/smart_ble_remote;c:/projects/peripheral_hids_keyboard;C:/projects/peripheral_hids_keyboard;C:/ncs/v2.6.0/nrf;C:/ncs/v2.6.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/projects/peripheral_hids_keyboard/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/projects/peripheral_hids_keyboard/boards/smart-remote-52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
